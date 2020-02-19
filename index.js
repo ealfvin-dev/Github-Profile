d321476f7423c6a777d1343b4f209c35919aae48
Erik Alfvin <erikalfvin@gmail.com>
2020-02-18 22:39:07 -0600

    Pulled useful data out of axios response

--- index.js
+++ index.js
@@ -17,7 +17,22 @@ inquirer.prompt(
     ]).then(function(input) {
         const config = { headers: { accept: "application/json" } };
 
-axios.get("https://api.github.com/users/" + input.username, config).then(function(response) {
-    console.log(response);
-    })
-})
+        axios.get("https://api.github.com/users/" + input.username, config).then(function(response) {
+            console.log(response);
+            const username = input.username;
+            const location = response.data.location;
+            const gitHub = response.html_url;
+            const blog = response.data.blog;
+            const bio = response.data.bio;
+            const numRepos = response.data.public_repos;
+            const numFollowers = response.data.followers;
+            const numFollowing = response.data.following;
+            const reposURL = response.data.repos_url;
+
+            axios.get(reposURL, config).then(function(repos) {
+                for(let item of repos.data) {
+                    console.log(item.stargazers_count);
+                }
+            });
+        });
+    });
