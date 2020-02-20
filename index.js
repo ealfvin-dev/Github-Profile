116f04062eb0200b4ef2c3d2e6e7f27961b55ba7
Erik Alfvin <erikalfvin@gmail.com>
2020-02-19 22:02:37 -0600

    Set up html output

--- index.js
+++ index.js
@@ -20,19 +20,29 @@ inquirer.prompt(
         axios.get("https://api.github.com/users/" + input.username, config).then(function(response) {
             console.log(response);
             const username = input.username;
+            const image = response.data.avatar_url;
+
             const location = response.data.location;
             const gitHub = response.html_url;
+
             const blog = response.data.blog;
             const bio = response.data.bio;
+
             const numRepos = response.data.public_repos;
+
             const numFollowers = response.data.followers;
             const numFollowing = response.data.following;
+
             const reposURL = response.data.repos_url;
 
+            const numStars = 0;
+
             axios.get(reposURL, config).then(function(repos) {
-                for(let item of repos.data) {
-                    console.log(item.stargazers_count);
+                for(let repo of repos.data) {
+                    numStars += repo.stargazers_count;
                 }
             });
+            
+
         });
     });
